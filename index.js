b57856f27de5429e08ca0048f2e47b07b2b80677
Aaron Levy <aaronlevypc21@gmail.com>
2023-12-11 23:09:39 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 function receivesAFunction(callback) {
-    const spy = "results"
-    callback(spy);
+    return callback();
 }
 
-const returnsANamedFunction = function () {
-    return function namedFunction() {
-      console.log("This is a named function");
-    };
-};
-const returnsAnAnonymousFunction = function(){
-    return function () {
+function returnsANamedFunction() {
+    return function returnsANamedFunction() {}
+}
 
-    }
+function returnsAnAnonymousFunction() {
+    return function () {}
 }
